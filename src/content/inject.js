7ee57ceb1916b61a02219fdfb854145865883fc7
NeuraiProject <code@neurai.org>
2026-03-09 09:41:36 +0100

    Bump version to 0.7.2 in package.json, inject.js, manifest.json, and popup.html

--- src/content/inject.js
+++ src/content/inject.js
@@ -45,7 +45,7 @@
   // Expose API to the page
   window.neuraiWallet = {
     isInstalled: true,
-    version: '0.7.1',
+    version: '0.7.2',
 
     getAddress: function() {
       return _request('getAddress');
@@ -77,6 +77,6 @@
 
   // Notify the page that the API is ready
   document.dispatchEvent(new CustomEvent('neuraiWalletReady', {
-    detail: { isInstalled: true, version: '0.7.1' }
+    detail: { isInstalled: true, version: '0.7.2' }
   }));
 })();
